96602777eb0bf74ff8c2e44927bba11a998552da
sekito <sekito1107gg@gmail.com>
2026-02-05 00:54:42 +0900

    Feat: EditorController (Stimulus) を実装

---
+++ app/javascript/controllers/editor_controller.js
@@ -0,0 +1,53 @@
+import { Controller } from "@hotwired/stimulus"
+import { EditorView, basicSetup } from "codemirror"
+import { ruby } from "@codemirror/lang-ruby"
+import { oneDark } from "@codemirror/theme-one-dark"
+import { Compartment } from "@codemirror/state"
+
+export default class extends Controller {
+  static targets = ["container"]
+
+  connect() {
+    this.themeConfig = new Compartment()
+
+    this.editor = new EditorView({
+      doc: "# Welcome to RubPad!\n# Type code here and see Rurema links appear on the right.\n\nnames = ['Ruby', 'Python', 'JavaScript']\n\nnames.select { |n| n.include?('u') }\n  .map(&:upcase)\n  .each do |n|\n    puts \"Hello, #{n}!\"\n  end\n\n# Try typing .split or .size below:\n",
+      extensions: [
+        basicSetup,
+        ruby(),
+        this.themeConfig.of(this.currentThemeExtension),
+        EditorView.lineWrapping
+      ],
+      parent: this.containerTarget
+    })
+
+    // Observe theme changes
+    this.observer = new MutationObserver(() => {
+      this.updateTheme()
+    })
+    this.observer.observe(document.documentElement, {
+      attributes: true,
+      attributeFilter: ["class"]
+    })
+  }
+
+  disconnect() {
+    if (this.editor) {
+      this.editor.destroy()
+    }
+    if (this.observer) {
+      this.observer.disconnect()
+    }
+  }
+
+  updateTheme() {
+    this.editor.dispatch({
+      effects: this.themeConfig.reconfigure(this.currentThemeExtension)
+    })
+  }
+
+  get currentThemeExtension() {
+    const isDark = document.documentElement.classList.contains("dark")
+    return isDark ? oneDark : []
+  }
+}
