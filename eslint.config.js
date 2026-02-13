ba0cc1c7c1f1d7003b01348fd82d4564184c1eb0
sekito <sekito1107gg@gmail.com>
2026-02-13 14:06:24 +0900

    プロジェクト設定の改善: セットアップ、一括テスト、リントの追加とCI更新

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    languageOptions: {
+      globals: {
+        browser: true,
+        node: true,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  }
+);
